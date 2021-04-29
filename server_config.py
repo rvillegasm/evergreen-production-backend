288ec23e1830e4988bdbf0bc79b8ba77a8970155
Rafael Villegas <rafa.villegas.michel@gmail.com>
2021-04-28 20:07:28 -0500

    Changed db config

--- server_config.py
+++ server_config.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://admin:pinguinos@evergreen-database-1.cjk96vzdjzzq.us-east-1.rds.amazonaws.com:3306/evergreen_database_1'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
