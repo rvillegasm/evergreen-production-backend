0e4e873d2baf3b1a5dde4e8cd0140a917cb29c4e
Rafael Villegas <rafa.villegas.michel@gmail.com>
2021-04-28 21:02:43 -0500

    More changes

--- server_config.py
+++ server_config.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://admin:pinguinos@evergreen-database-1.cjk96vzdjzzq.us-east-1.rds.amazonaws.com:3306/evergreen_database_1'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
